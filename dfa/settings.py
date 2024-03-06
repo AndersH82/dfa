b1a0d8c846dd0efd8cd26ec8174c0ea7cf12ef87
Anders Holst <andersthereseholst@gmail.com>
2024-03-06 08:17:24 +0000

    added image_filter choice field and validation

--- dfa/settings.py
+++ dfa/settings.py
@@ -19,7 +19,7 @@ CLOUDINARY_STORAGE = {
     'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
 }
 MEDIA_URL = '/media/'
-DEFAULT_FILES_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
