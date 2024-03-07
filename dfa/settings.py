dff205d1d95bbeafc9efe0e0ae593ac126fc4cff
Anders Holst <andersthereseholst@gmail.com>
2024-03-07 16:44:12 +0000

    change in allowed hosts

--- dfa/settings.py
+++ dfa/settings.py
@@ -59,10 +59,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
+
 ALLOWED_HOSTS = [
-    os.environ.get('ALLOWED_HOST'),
-    '8000-andersh82-dfa-763n415494s.ws-eu108.gitpod.io',
-    ]
+    *os.environ.get('ALLOWED_HOST', '').split(' '),
+    '8000-andersh82-dfa-763n415494s.ws-eu108.gitpod.io'
+]
 
 
 # Application definition
