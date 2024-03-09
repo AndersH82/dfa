63ce46008c889bfa3c9094f13f539ea9a49edf49
Anders Holst <andersthereseholst@gmail.com>
2024-03-09 14:10:44 +0000

    changes

--- dfa/settings.py
+++ dfa/settings.py
@@ -33,7 +33,7 @@ REST_FRAMEWORK = {
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )],
     'DEFAULT_PAGINATION_CLASS':
-        'rest_framework.pagination.PageNumberPagination',
+    'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DATETIME_FORMAT': '%d %b %Y',
 }
@@ -46,9 +46,10 @@ REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+JWF_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER': 'dfa_api.serializers.CurrentUserSerializer'
+    'USER_DETAILS_SERIALIZER': 'dfa.serializers.CurrentUserSerializer'
 }
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
