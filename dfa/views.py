0337b64fa7f904f9af148d6a386bd8ac0427512e
Anders Holst <andersthereseholst@gmail.com>
2024-03-13 09:32:20 +0000

    settings

--- dfa/views.py
+++ dfa/views.py
@@ -1,10 +1,23 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from .settings import (
     JWT_AUTH_COOKIE, JWT_AUTH_REFRESH_COOKIE, JWF_AUTH_SAMESITE,
     JWT_AUTH_SECURE,
 )
 
+
+class ExampleView(api_view):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        content = {
+            'user': str(request.user),  # `django.contrib.auth.User` instance.
+            'auth': str(request.auth),  # None
+        }
+        return Response(content)
 
 @api_view()
 def root_route(request):
